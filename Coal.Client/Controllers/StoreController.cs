d92fb7a67066ff84d0f128357d0cf479ce4fa351
earl <acearl57@gmail.com>
2020-08-20 19:09:02 -0400

    added files for future development

---
+++ Coal.Client/Controllers/StoreController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Coal.Client.Models;
+
+namespace Coal.Client.Controllers
+{
+}
