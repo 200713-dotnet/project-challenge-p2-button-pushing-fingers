cd588ddd52143dbe8bb2209975bd2e03caee6ba8
david <ddawkins417@gmail.com>
2020-08-20 20:12:27 -0500

    Added additional functions to UserRepo

--- Coal.Storing/Repositories/PublisherRepo.cs
+++ Coal.Storing/Repositories/PublisherRepo.cs
@@ -1,5 +1,18 @@
+using System.Linq;
+using Coal.Storing.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Coal.Storing.Repositories
 {
+  
+  // TODO //
+  /*
+  - Read all games/mods/dlc for a publisher
+  - Create a game/mod/dlc for the marketplace
+  - Update a game/mod/dlc in the marketplace
+  - Delete a game/mod/dlc (may not need)
+  */
+  //////////
   public class PublisherRepo
   {
     private CoalDbContext _db;
@@ -9,14 +22,32 @@ namespace Coal.Storing.Repositories
       _db = dbContext;
     }
 
-    public void Create()
+    public Publisher Create(string name)
     {
-
+      Publisher publisher = new Publisher(){ Name = name };
+      _db.Publishers.Add(publisher);
+      _db.SaveChanges();
+      return publisher;
     }
 
-    public void Read()
+    public Publisher Read(string name)
     {
+      return _db.Publishers
+        .Where(p => p.Name == name)
+        .Include(p => p.Games)
+        .Include(p => p.Mods)
+        .Include(p => p.DownloadableContents)
+        .FirstOrDefault();
+    }
 
+    public Publisher Read(int id)
+    {
+      return _db.Publishers
+        .Where(p => p.Id == id)
+        .Include(p => p.Games)
+        .Include(p => p.Mods)
+        .Include(p => p.DownloadableContents)
+        .FirstOrDefault();
     }
 
     public void Update()
