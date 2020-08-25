af55b04fdfe278b24165f9f092764e9a429caf34
david <ddawkins417@gmail.com>
2020-08-25 14:18:18 -0500

    Added code analysis

--- Coal.Storing/Models/Library.cs
+++ Coal.Storing/Models/Library.cs
@@ -5,7 +5,7 @@ namespace Coal.Storing.Models
   public class Library : SModel
   {
     public int UserId { get; set; }
-    public User User { get; set; }
+    public User User { get; set; }  //May need to remove
     public List<LibraryGame> LibraryGames { get; set; }
     public List<LibraryDLC> LibraryDLCs { get; set; }
     public List<LibraryMod> LibraryMods { get; set; }
---
+++ dockerfile
@@ -0,0 +1,9 @@
+FROM mcr.microsoft.com/dotnet/core/sdk as publish
+WORKDIR /app
+COPY . .
+RUN dotnet publish -c Release -o out Coal.Client/Coal.Client.csproj
+
+FROM mcr.microsoft.com/dotnet/core/aspnet
+WORKDIR /dist
+COPY --from=publish /app/out /dist
+CMD [ "dotnet", "Coal.Client.dll" ]
